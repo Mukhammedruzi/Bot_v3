f018a83c1964edb3c06324c5fbd6c50683a0d90f
Mukhammedruzi <mukhammedruziqosimov968@gmail.com>
2026-08-02 13:32:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,27 @@
 # Bot_v3 configuration
+# config.py
+
+# YouTube API Key
+YOUTUBE_API_KEY = "AIzaSyD03QCX30h71YU8Sbmbig8UjIZkOE_IQQg"
+
+
+# Telegram Bot Token
+TELEGRAM_TOKEN = "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"
+
+
+# Qidiruv sozlamalari
+SEARCH_KEYWORDS = [
+    "PUBG Mobile redeem code",
+    "PUBG Mobile code",
+    "MLBB redeem code",
+    "Mobile Legends redeem code"
+]
+
+
+# Tekshirish oralig'i (sekundlarda)
+CHECK_INTERVAL = 3600
+
+
+# Fayllar
+CODES_FILE = "scanner/codes.txt"
+OLD_CODES_FILE = "scanner/old_codes.txt"
